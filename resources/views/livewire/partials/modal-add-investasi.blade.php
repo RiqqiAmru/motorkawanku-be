4c34c240626da585c91800ea2e7902c605cb9da1
RiqqiAmru <Riqqia00@gmail.com>
2024-10-25 13:29:36 +0700

    [temp] problem head detached

--- resources/views/livewire/partials/modal-add-investasi.blade.php
+++ resources/views/livewire/partials/modal-add-investasi.blade.php
@@ -110,34 +110,6 @@
         <div class="mt-6 ">
         </div>
 
-        {{-- <div class="mt-6 flex" x-data="{
-            role: '',
-            open: true
-        }">
-            <x-input-label for="role" value="{{ __('Role') }}"
-                class="inline-flex items-center px-4 py-2 font-semibold text-2xl" />
-            <select name="role" id="role" x-model="role"
-                class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                <option value="user" @if (old('role') == 'user') selected @endif @click="open = true">User
-                </option>
-                <option value="admin" @if (old('role') == 'admin') selected @endif @click="open = false">Admin
-                </option>
-            </select>
-            <template x-if="open">
-                <x-input-label for="kawasan" value="{{ __('Kawasan') }}"
-                    class="inline-flex items-center px-4 py-2 font-semibold text-2xl" />
-            </template>
-            <template x-if="open">
-                <select name="kawasan" id="kawasan" required
-                    class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm">
-                    <option>{{ __('') }}</option>
-                    @foreach ($kawasan as $key => $item)
-                        <option value="{{ $key }}" @if (old('kawasan') == $key) selected @endif>
-                            {{ $item }}</option>
-                    @endforeach
-                </select>
-            </template>
-        </div> --}}
 
         <div class="mt-6 flex justify-end">
             <x-secondary-button x-on:click="$dispatch('close')">
