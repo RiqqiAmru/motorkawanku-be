d19faea9786f5ac507486dda73635a106ddeee5e
RiqqiAmru <Riqqia00@gmail.com>
2024-11-04 15:33:30 +0700

    [perkim] adding logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,10 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
-    <style>
+    {{-- <style>
         /* ! tailwindcss v3.4.1 | MIT License | https://tailwindcss.com */
         *,
         ::after,
@@ -899,18 +900,18 @@
                 --tw-ring-color: rgb(255 255 255 / var(--tw-ring-opacity))
             }
         }
-    </style>
+    </style> --}}
 </head>
 
-<body class="font-sans antialiased dark:bg-black/55 dark:text-white/50 bg-fixed object-cover "
+<body class="font-sans antialiased bg-fixed object-cover "
     style="background-position:center; background-size: cover; background-image:url(/pekalongan.jpg)">
-    <div class="bg-gray-50 text-black/50 dark:bg-black/55 dark:text-white/50">
+    <div class=" text-black/50  dark:text-white/50">
         <div
-            class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
-            <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                    <div class="flex lg:justify-center lg:col-start-2">
-                        <img src="/kotaPekalongan.png" alt="logo kota pekalongan" class="h-12">
+            class="relative min-h-screen  flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
+            <div class="relative w-full max-w-2xl min-h-screen flex flex-col justify-between px-6 lg:max-w-7xl">
+                <header class="grid grid-cols-2 items-center gap-2 py-10 mt-10">
+                    <div class="flex lg:justify-start ">
+                        <img src="/kotaPekalongan.png" alt="logo kota pekalongan" class="h-24">
                     </div>
                     @if (Route::has('login'))
                         <nav class="-mx-3 flex flex-1 justify-end">
@@ -931,44 +932,21 @@
                 </header>
 
                 <main class="mt-6">
-                    <div class="grid gap-6 lg:gap-8">
 
-                        <a href="/guest" id="docs-card"
-                            class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                            <div class="relative flex items-center gap-6 lg:items-end">
-                                <div id="docs-card-content" class="flex items-start gap-6">
-                                    <div
-                                        class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
-                                        <svg class="size-5 sm:size-6" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                            viewBox="0 0 24 24">
-                                            <path fill="#FF2D20"
-                                                d="M23 4a1 1 0 0 0-1.447-.894L12.224 7.77a.5.5 0 0 1-.448 0L2.447 3.106A1 1 0 0 0 1 4v13.382a1.99 1.99 0 0 0 1.105 1.79l9.448 4.728c.14.065.293.1.447.1.154-.005.306-.04.447-.105l9.453-4.724a1.99 1.99 0 0 0 1.1-1.789V4ZM3 6.023a.25.25 0 0 1 .362-.223l7.5 3.75a.251.251 0 0 1 .138.223v11.2a.25.25 0 0 1-.362.224l-7.5-3.75a.25.25 0 0 1-.138-.22V6.023Zm18 11.2a.25.25 0 0 1-.138.224l-7.5 3.75a.249.249 0 0 1-.329-.099.249.249 0 0 1-.033-.12V9.772a.251.251 0 0 1 .138-.224l7.5-3.75a.25.25 0 0 1 .362.224v11.2Z" />
-                                            <path fill="#FF2D20"
-                                                d="m3.55 1.893 8 4.048a1.008 1.008 0 0 0 .9 0l8-4.048a1 1 0 0 0-.9-1.785l-7.322 3.706a.506.506 0 0 1-.452 0L4.454.108a1 1 0 0 0-.9 1.785H3.55Z" />
-                                        </svg>
-                                    </div>
-
-                                    <div class="pt-3 sm:pt-5 lg:pt-0">
-
-                                        <h2 class="text-xl font-semibold text-black dark:text-white">Motor Kawanku | E
-                                            Monitoring
-                                            Kawasan Permukiman Kumuh Kota Pekalongan</h2>
-                                        <p class="mt-4 text-sm/relaxed">
-                                            Motor Kawanku adalah website yang menyediakan data kawasan kumuh kota
-                                            pekalongan yang dikelompokkan berdasarkan PERMEN PUPR No.14 Tahun 2018
-                                            Tentang Kawasan Kumuh
-                                        </p>
-                                    </div>
-                                </div>
-
-                                <svg class="size-6 shrink-0 stroke-[#FF2D20]" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 24 24" stroke-width="1.5">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                                </svg>
-                            </div>
-                        </a>
-
+                    <div class="pb-12 text-center md:pb-16">
+                        <div class="flex justify-center my-4">
+                            <img src="/logo.jpg" alt="logo motorkawanku" class="h-24 drop-shadow-sm">
+                        </div>
+                        <h1 class="mb-6 drop-shadow-lg my-4 text-5xl font-extrabold text-red-600  md:text-9xl ">Motor
+                            <span class="underline decoration-sky-500 text-white">Kawanku</span>
+                        </h1>
+                        <div class="mx-auto max-w-3xl bg-slate-200/15 rounded-full p-2">
+                            <p class="mb-8 text-lg text-white drop-shadow-md ">E-Monitoring
+                                Kawasan
+                                Permukiman
+                                Kumuh Kota Pekalongan</p>
+                            <x-primary-button>Lihat</x-primary-button>
+                        </div>
                     </div>
                 </main>
 
@@ -982,6 +960,9 @@
             </div>
         </div>
     </div>
+
+
+
 </body>
 
 </html>
